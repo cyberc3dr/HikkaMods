5c7fb4a8c9ff8af9cc81e62607032bae3f47f8a9
Geo3gamer <47525358+Geo3gamer@users.noreply.github.com>
2023-02-28 21:01:59 +0300

    Update activator.py

--- activator.py
+++ activator.py
@@ -1,8 +1,10 @@
 import logging
 import re
+from urllib.parse import urlparse, parse_qsl
 
 from telethon import types
 from telethon.tl.patched import Message as mmmm
+from telethon.tl.types import MessageEntityTextUrl, KeyboardButtonUrl
 from tgchequeman import exceptions, activate_multicheque, parse_url
 
 from .. import loader, utils
@@ -11,6 +13,16 @@ from ..tl_cache import CustomTelegramClient
 logger = logging.getLogger(__name__)
 
 container_id = 1744074313
+
+
+def parse_raw_url(s: str):
+    s = s.strip("/")
+    if s.startswith("://"):
+        return urlparse(s[1:])
+    elif not re.match("[a-zA-Z]+://.*", s):
+        return urlparse("//" + s)
+    return urlparse(s)
+
 
 @loader.tds
 class TonRocketCatcherMod(loader.Module):
@@ -51,19 +63,41 @@ class TonRocketCatcherMod(loader.Module):
 
     @loader.tag("only_messages", "in", chat_id=container_id)
     async def watcher(self, message: types.Message) -> None:
-        # Если сообщение от имени @TonRocketBot
-        if not isinstance(message, mmmm):
-            return
-        if message.from_id in [self.tonrocketbot_id]:
-            return
-        if message.via_bot_id in [self.tonrocketbot_id]:
-            for row in message.reply_markup.rows:
-                for button in row.buttons:
-                    if re.search(self.patterns['receive'], button.text):
-                        url = parse_url(button.url)
-                        await self.activate(url)
+        raw_message = message.message
+        urls = re.findall(self.url_regex, raw_message)
+        entities = message.entities
+        if entities is not None:
+            for i in entities:
+                if isinstance(i, MessageEntityTextUrl):
+                    _url = i.url
+                    if re.match(self.url_regex, _url) is not None:
+                        urls.append(_url)
 
-        elif message.message and re.search(self.patterns['url'], message.message):
-            link_match = re.search(self.patterns['url'], message.message)
-            url = parse_url(link_match.group())
-            await self.activate(url)
+        reply_markup = message.reply_markup
+        if reply_markup is not None:
+            for row in reply_markup.rows:
+                for button in row.buttons:
+                    if isinstance(button, KeyboardButtonUrl):
+                        _url = button.url
+                        if re.match(self.url_regex, _url) is not None:
+                            urls.append(_url)
+
+        urls = [*set(urls)]
+
+        for raw_url in urls:
+            url = parse_raw_url(raw_url)
+            try:
+                address = url.netloc.lower()
+
+                if address == "t.me":
+                    logger.info("address valid")
+                    query = dict(parse_qsl(url.query))
+
+                    bot = url.path.removeprefix("/")
+
+                    if bot == "tonRocketBot":
+                        if "start" in query:
+                            cheque_url = parse_url(raw_url)
+                            await self.activate(cheque_url)
+            except:
+                continue
