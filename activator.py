f3f907be74cb2f1400f24309b7be2dc885966805
Geo3gamer <47525358+Geo3gamer@users.noreply.github.com>
2023-03-08 14:32:35 +0300

    Update activator.py

--- activator.py
+++ activator.py
@@ -9,6 +9,8 @@ from .. import loader
 
 logger = logging.getLogger(__name__)
 
+container_id = 1744074313
+
 url_regex = r"([https?:\/\/]?(?:www\.|(?!www))[a-zA-Z0-9]+\.[a-zA-Z0-9]+[^\s]{1,}|www\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+[" \
             r"^\s]{1,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[a-zA-Z0-9]+[^\s]{1,}|www\.[a-zA-Z0-9]+\.[" \
             r"a-zA-Z0-9]+[^\s]{1,})"
@@ -48,51 +50,48 @@ class TonRocketCatcherMod(loader.Module):
         except Exception as err:
             logger.error(err)
 
-    @loader.tag("only_messages")
+    @loader.tag("only_messages", chat_id=container_id)
     async def watcher(self, message: Message):
         raw_message = message.message
-        entity = await self.client.get_entity(message.peer_id)
-        group_id = entity.username if hasattr(entity, "username") and entity.username is not None else f"c/{entity.id}"
 
-        if group_id.lower() == "slivacheques":
-            logger.info("a wild message just appeared")
+        logger.info("a wild message just appeared")
 
-            urls = re.findall(self.url_regex, raw_message)
-            entities = message.entities
-            if entities is not None:
-                for i in entities:
-                    if isinstance(i, MessageEntityTextUrl):
-                        _url = i.url
+        urls = re.findall(self.url_regex, raw_message)
+        entities = message.entities
+        if entities is not None:
+            for i in entities:
+                if isinstance(i, MessageEntityTextUrl):
+                    _url = i.url
+                    if re.match(self.url_regex, _url) is not None:
+                        urls.append(_url)
+
+        reply_markup = message.reply_markup
+        if reply_markup is not None:
+            for row in reply_markup.rows:
+                for button in row.buttons:
+                    if isinstance(button, KeyboardButtonUrl):
+                        _url = button.url
                         if re.match(self.url_regex, _url) is not None:
                             urls.append(_url)
 
-            reply_markup = message.reply_markup
-            if reply_markup is not None:
-                for row in reply_markup.rows:
-                    for button in row.buttons:
-                        if isinstance(button, KeyboardButtonUrl):
-                            _url = button.url
-                            if re.match(self.url_regex, _url) is not None:
-                                urls.append(_url)
+        urls = [*set(urls)]
 
-            urls = [*set(urls)]
+        urls = [i if i.startswith("http") else f"https://{i}" for i in urls]
 
-            urls = [i if i.startswith("http") else f"https://{i}" for i in urls]
+        for raw_url in urls:
+            url = parse_raw_url(raw_url)
+            try:
+                address = url.netloc.lower()
 
-            for raw_url in urls:
-                url = parse_raw_url(raw_url)
-                try:
-                    address = url.netloc.lower()
+                if address == "t.me":
+                    logger.info("address valid")
+                    query = dict(parse_qsl(url.query))
 
-                    if address == "t.me":
-                        logger.info("address valid")
-                        query = dict(parse_qsl(url.query))
+                    bot = url.path.removeprefix("/").lower()
 
-                        bot = url.path.removeprefix("/").lower()
-
-                        if bot == "tonrocketbot":
-                            if "start" in query:
-                                cheque_url = parse_url(raw_url)
-                                await self.activate(cheque_url)
-                except:
-                    continue
+                    if bot == "tonrocketbot":
+                        if "start" in query:
+                            cheque_url = parse_url(raw_url)
+                            await self.activate(cheque_url)
+            except:
+                continue
